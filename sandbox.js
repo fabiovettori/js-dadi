5cf233b3c5d7f972bcdd47c76fa1be3aa5a1ad72
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-14 14:59:33 +0200

    General settings

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+// Esercitazione 2: Gioco dei dadi
+// Generare un numero casuale tra 1 e 6 per simulare il lancio di un dado (giocata 1).
+// Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
+// Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
